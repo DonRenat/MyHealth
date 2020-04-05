902808fd330ba1ab7d02be48aca2c6b95621d2bb
Renat Gasanov <ren99999@mail.ru>
2020-04-05 22:51:03 +1000

    Initial Commit

---
+++ MyHealth/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  MyHealth
+//
+//  Created by Renat Gasanov on 23.10.2019.
+//  Copyright © 2019 Renat Gasanov. All rights reserved.
+//
+
+import UIKit
+import Comets
+import Cards
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let width = view.bounds.width
+        let height = view.bounds.height
+        let comets = [Comet(startPoint: CGPoint(x: 100, y: 0),
+                            endPoint: CGPoint(x: 0, y: 100),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.white),
+                      Comet(startPoint: CGPoint(x: 0.4 * width, y: 0),
+                            endPoint: CGPoint(x: width, y: 0.8 * width),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.white),
+                      Comet(startPoint: CGPoint(x: 0.8 * width, y: 0),
+                            endPoint: CGPoint(x: width, y: 0.2 * width),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.white),
+                      Comet(startPoint: CGPoint(x: width, y: 0.2 * height),
+                            endPoint: CGPoint(x: 0, y: 0.25 * height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.white),
+                      Comet(startPoint: CGPoint(x: 0, y: height - 0.8 * width),
+                            endPoint: CGPoint(x: 0.6 * width, y: height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.white),
+                      Comet(startPoint: CGPoint(x: width - 100, y: height),
+                            endPoint: CGPoint(x: width, y: height - 100),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.white),
+                      Comet(startPoint: CGPoint(x: 0, y: 0.8 * height),
+                            endPoint: CGPoint(x: width, y: 0.75 * height),
+                            lineColor: UIColor.white.withAlphaComponent(0.2),
+                            cometColor: UIColor.white)]
+
+        // draw line track and animate
+        for comet in comets {
+            view.layer.addSublayer(comet.drawLine())
+            view.layer.addSublayer(comet.animate())
+        }
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MyHealth' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MyHealth
+  pod "Comets"
+  pod 'Cards'
+
+post_install do |installer|
+  installer.pods_project.build_configurations.each do |config|
+    config.build_settings['LD_RUNPATH_SEARCH_PATHS'] = [
+      '$(FRAMEWORK_SEARCH_PATHS)'
+    ]
+    installer.pods_project.build_configurations.each do |config|
+        config.build_settings.delete('CODE_SIGNING_ALLOWED')
+        config.build_settings.delete('CODE_SIGNING_REQUIRED')
+   end
+end
+end
+
+end
