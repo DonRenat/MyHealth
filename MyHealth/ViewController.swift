6bf4fed622e6dabc1416c8e689e391c0ba491b84
Renat Gasanov <ren99999@mail.ru>
2020-04-07 05:01:31 +1000

    piChart for status.

--- MyHealth/ViewController.swift
+++ MyHealth/ViewController.swift
@@ -13,12 +13,17 @@ import Charts
 class ViewController: UIViewController {
     
     @IBOutlet weak var Chart: LineChartView!
+    @IBOutlet weak var pieChart: PieChartView!
     @IBOutlet weak var dataSelector: UISegmentedControl!
     
     //future update: take data from server
     var temperature : [Double] = [36.7, 37.0, 38.2, 39.3, 37.0, 36.6]
     var pulse : [Double] = [66, 80, 73, 55, 59, 90]
-    var feeling : [Double] = [1, 2, 3, 4, 5, 6]
+    
+    //test feeling
+    //var feeling : [Double] = [1, 2, 3, 4, 5, 6]
+    let status = ["Good", "Medium", "Bad", "SOS"]
+    let statusAmount = [10, 13, 4, 1]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,25 +64,34 @@ class ViewController: UIViewController {
             view.layer.addSublayer(comet.animate())
         }
         
+        pieChart.isHidden = true
+        //updatePieChart(dataPoints: status, values: statusAmount.map{ Double($0) })
+        
         Chart.rightAxis.enabled = false
         Chart.xAxis.enabled = false
-        updateGraph(data: temperature)
+        updateLineChart(data: temperature)
     }
 
     @IBAction func indexChanged(_ sender: Any) {
         switch dataSelector.selectedSegmentIndex{
         case 0:
-            updateGraph(data: temperature)
+            Chart.isHidden = false
+            pieChart.isHidden = true
+            updateLineChart(data: temperature)
         case 1:
-            updateGraph(data: pulse)
+            Chart.isHidden = false
+            pieChart.isHidden = true
+            updateLineChart(data: pulse)
         case 2:
-            updateGraph(data: feeling)
+            updatePieChart(dataPoints: status, values: statusAmount.map{ Double($0) })
+            Chart.isHidden = true
+            pieChart.isHidden = false
         default:
             break
         }
     }
     
-    func updateGraph(data: [Double]){
+    func updateLineChart(data: [Double]){
         var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
         
         for i in 0..<data.count {
@@ -97,6 +111,46 @@ class ViewController: UIViewController {
         Chart.data = data
         //Chart.legend.enabled = false
         //Chart.chartDescription?.text = description
+    }
+    
+    func updatePieChart(dataPoints: [String], values: [Double]) {
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+          let dataEntry = PieChartDataEntry(value: values[i], label: dataPoints[i], data: dataPoints[i] as AnyObject)
+          dataEntries.append(dataEntry)
+        }
+
+        let pieChartDataSet = PieChartDataSet(entries: dataEntries, label: nil)
+        
+        //pieChartDataSet.colors = colorsOfCharts(numbersOfColor: dataPoints.count)
+        var  colors: [UIColor] = []
+        colors.append(UIColor.green)
+        colors.append(UIColor.orange)
+        colors.append(UIColor.red)
+        colors.append(UIColor.magenta)
+        //pieChartDataSet.colors = colors
+        pieChartDataSet.colors = colorsOfCharts(numbersOfColor: 4)
+        
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        let format = NumberFormatter()
+        format.numberStyle = .none
+        let formatter = DefaultValueFormatter(formatter: format)
+        pieChartData.setValueFormatter(formatter)
+
+        pieChart.data = pieChartData
+    }
+    
+    private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
+      var colors: [UIColor] = []
+      for _ in 0..<numbersOfColor {
+        let red = Double(arc4random_uniform(256))
+        let green = Double(arc4random_uniform(256))
+        let blue = Double(arc4random_uniform(256))
+        let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+        colors.append(color)
+      }
+      return colors
     }
 }
 
