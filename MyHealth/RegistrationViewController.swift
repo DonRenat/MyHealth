c1b720dba6cd26bd60165313ae388a2fb1192322
Renat Gasanov <ren99999@mail.ru>
2020-04-13 09:23:02 +1000

    Registration form now has one fully done textfield using SkyFloatingLabelTextField pod.

--- MyHealth/RegistrationViewController.swift
+++ MyHealth/RegistrationViewController.swift
@@ -7,21 +7,29 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField
 
-class RegistrationViewController: UIViewController {
+class RegistrationViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var text1: SkyFloatingLabelTextField!
     @IBOutlet weak var doneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.text1.delegate = self
+        
         doneButton.layer.cornerRadius = 15
         doneButton.layer.cornerCurve = .continuous
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func doneButtonClick(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
     
 }
