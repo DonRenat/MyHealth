288afa69dc42e88ce78cf22c71a0e2d7afe0913a
Renat Gasanov <ren99999@mail.ru>
2020-04-14 09:10:52 +1000

    Added app icon. Minor fixes in settings.

--- MyHealth/RegistrationViewController.swift
+++ MyHealth/RegistrationViewController.swift
@@ -11,13 +11,16 @@ import SkyFloatingLabelTextField
 
 class RegistrationViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var text1: SkyFloatingLabelTextField!
+    @IBOutlet weak var nameTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var famTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var snTextField: SkyFloatingLabelTextField!
     @IBOutlet weak var doneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.text1.delegate = self
+        nameTextField.delegate = self
+        famTextField.delegate = self
         
         doneButton.layer.cornerRadius = 15
         doneButton.layer.cornerCurve = .continuous
@@ -26,10 +29,4 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
     @IBAction func doneButtonClick(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.endEditing(true)
-        return false
-    }
-    
 }
